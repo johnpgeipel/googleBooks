319be93e4c24923a49e7c515f3944e6af7a35344
Johnny Rocksalt <9876543+johnpgeipel@users.noreply.github.com>
2019-09-25 12:38:24 -0500

    fixed homepage code

---
+++ client/build/precache-manifest.8ce0008ee0c6724f0fb4d056f0367996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebfb03d8fcf059c730ba8c23b0f1685f",
+    "url": "/static/media/circles-light.ebfb03d8.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a403e37d6dcf13fbf860",
+    "url": "/static/js/main.a403e37d.chunk.js"
+  },
+  {
+    "revision": "816bea2dd231c72349f3",
+    "url": "/static/js/1.816bea2d.chunk.js"
+  },
+  {
+    "revision": "a403e37d6dcf13fbf860",
+    "url": "/static/css/main.08cfcaca.chunk.css"
+  },
+  {
+    "revision": "5146d5c5ca805f1e08f09c13188e231a",
+    "url": "/index.html"
+  }
+];
